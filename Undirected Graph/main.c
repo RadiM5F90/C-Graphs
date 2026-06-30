7c910837d1d2c7d1dc3fbf311a201aead012b7f9
Radimir <radimir@rdmautomations.com>
2026-06-30 14:57:40 +0200

    Refactor main.c for undirected graph implementation

--- Undirected Graph/main.c
+++ Undirected Graph/main.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
-#include "directed_graph.h"
+#include "undirected_graph.h"
 
 
 void print_node_list(const list _list) {
     iterator it = iterator_create(_list);
-
-    while (has_iterator_next(it)) {
+    while (iterator_has_next(it)) {
         int id = iterator_next(it);
         printf("%d ", id);
     }
-    destroy_iterator(&it);
+    iterator_destroy(&it);
     printf("\n");
 }
 
-
 int main() {
-    direct_graph g = direct_graph_create(10);
+    indirect_graph g = indirect_graph_create(100);
 
-    node_id a = add_node(g, 10);
-    node_id b = add_node(g, 20);
-    node_id c = add_node(g, 30);
-    node_id d = add_node(g, 40);
-    node_id e = add_node(g, 50);
-
+    int a = add_node(g, 10);
+    int b = add_node(g, 20);
+    int c = add_node(g, 30);
+    int d = add_node(g, 40);
+    int e = add_node(g, 50);
 
     add_edge(g, a, b);
     add_edge(g, a, c);
@@ -31,34 +28,27 @@ int main() {
     add_edge(g, d, e);
 
     printf("DFS from node %d: ", a);
-    list _dfs = direct_graph_dfs(g, a);
-    print_node_list(_dfs);
-    destroy_list(&_dfs);
+    list dfs = indirect_graph_dfs(g, a);
+    print_node_list(dfs);
+    list_destroy(&dfs);
 
     printf("BFS from node %d: ", a);
-    list _bfs = direct_graph_bfs(g, a);
-    print_node_list(_bfs);
-    destroy_list(&_bfs);
+    list bfs = indirect_graph_bfs(g, a);
+    print_node_list(bfs);
+    list_destroy(&bfs);
 
-    // getting the value
+
     int value;
-    if (get_value(g, d, &value) == 0) printf("\nNode's %d value: %d\n", d, value);
+    if (indirect_graph_get_value(g, d, &value) == 0) printf("Node's %d value: %d\n", d, value);
 
-
-    list neighbors = direct_graph_neighbors(g, b);
+    list neighbors = indirect_graph_neighbors(g, b);
     printf("Node's %d neighbors: ", b);
     print_node_list(neighbors);
-    destroy_list(&neighbors);
+    list_destroy(&neighbors);
 
-    // checking the path
-    printf("path from %d to %d: %s\n", a, e,
-        direct_graph_path_exists(g, a, e) ? "Exists" : "Not exists");
+    printf("Path from %d to %d: %s\n", a, e, indirect_graph_path_exists(g, a, e) ? "Exists" : "Does not exist");
 
-    printf("path from %d to %d: %s\n", e, a,
-        direct_graph_path_exists(g, e, a) ? "Exists" : "Not exists");
-
-    direct_graph_destroy(&g);
+    indirect_graph_destroy(&g);
 
     return 0;
 }
-
